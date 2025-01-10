4eb9fe5c828089f3e9a48b1f97c7fede8a9a0a1a
Vitor Baiense <vitorbaiense@gmail.com>
2025-01-10 18:02:27 -0300

    nova pratica

--- cypress/e2e/pratica1.cy.js
+++ cypress/e2e/pratica1.cy.js
@@ -4,7 +4,7 @@ describe('acessando diferentes páginas do ADOPET', () => {
       cy.contains('a', 'Ver pets disponíveis para adoção').click();
     });
 
-    it('Deve acessar a página inicial e cliclar no botão "Ver pets disponíveis para adoção”, Visite a página de principal do AdoPet e teste os botões header, Visite a página de /login do Adopet, Visite a página de /home do Adopet, Visite a página de /home do AdoPet e clique no botão “Falar com o responsável”', () => {
+    it('Deve acessar a página inicial e clicar no botão "Ver pets disponíveis para adoção”, Visite a página de principal do AdoPet e teste os botões header, Visite a página de /login do Adopet, Visite a página de /home do Adopet, Visite a página de /home do AdoPet e clique no botão “Falar com o responsável”', () => {
       cy.wait(2000);
       cy.get('a[href="/"]').click();
       cy.wait(2000);
@@ -12,8 +12,9 @@ describe('acessando diferentes páginas do ADOPET', () => {
       cy.wait(2000);
       cy.get('a[href="/"]').click();
       cy.contains('a', 'Fazer login').click();
-      cy.get('input[name="email"]').type('vitorbaiense@gmail.com');
-      cy.get('input[name="password"]').type('Vitor1234');
+      cy.login('vitorbaiense@gmail.com', 'Vitor1234');
+      // cy.get('input[name="email"]').type('vitorbaiense@gmail.com');
+      // cy.get('input[name="password"]').type('Vitor1234');
       cy.contains('button', 'Entrar').click();
       cy.wait(4000);
       cy.get('div[class="card"]').contains('a', 'Falar com responsável').click();
