6eda3d8044a665a0324ea45c0c1906b81147fe51
Vitor Baiense <vitorbaiense@gmail.com>
2025-10-22 15:34:03 -0300

    ultimos exercicios realizados no curso de Cypress

--- cypress/e2e/api-mensagens.cy.js
+++ cypress/e2e/api-mensagens.cy.js
@@ -1,5 +1,5 @@
 describe('API AdoPet', () => {
-    const authorization = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI5MDJkMDlkNS0yZTVkLTRmOWUtOWMyMy04MzVmNmIzYWI4NmMiLCJhZG9wdGVyTmFtZSI6InZpdG9yIGJhaWVuc2UiLCJpYXQiOjE3NDM4NTg0NDcsImV4cCI6MTc0NDExNzY0N30.kLAToulyRygT1mCAiMm190sREpD_I5aaq0a8C1-BMfE'
+    const tempoEsperado = Math.random() * 1000;
 
     beforeEach(() => {
         // Escreva o código que será executado antes de cada teste
@@ -9,11 +9,13 @@ describe('API AdoPet', () => {
         cy.request({
             method: 'GET',
             url: 'https://adopet-api-i8qu.onrender.com/mensagem/902d09d5-2e5d-4f9e-9c23-835f6b3ab86c',
-            headers: { authorization}
+            headers: Cypress.env()
         }).then((res) => {
             expect(res.status).to.be.eq(200);
             expect(res.body).is.not.empty;
             expect(res.body).to.have.property('msg');
+            // flaky test
+            expect(res.duration).to.be.lte(tempoEsperado);
         });
     });
 });
